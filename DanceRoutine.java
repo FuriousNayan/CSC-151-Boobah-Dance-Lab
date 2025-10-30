6adb952fbcb89bd8d72850165a3f3d18d8a4b7c8
FuriousNayan <npatel24@my.waketech.edu>
2025-10-30 09:17:31 -0400

    pushing all

--- DanceRoutine.java
+++ DanceRoutine.java
@@ -17,4 +17,15 @@ public class DanceRoutine {
         String newRoutine = this.buildRoutine().replace("performs", "dances to");
         return newRoutine;
     }
+
+    public String remixRoutine(){
+        String newRoutine = this.buildRoutine().replace("performs", "dances to");
+        StringBuilder remixedRoutineBuilder = new StringBuilder();
+        remixedRoutineBuilder.insert(0,"---REMIX---\n");
+        remixedRoutineBuilder.append(newRoutine);
+        remixedRoutineBuilder.delete(remixedRoutineBuilder.length() - 1, remixedRoutineBuilder.length());
+        remixedRoutineBuilder.append("\n(Backwards Boohbah Shuffle!)");
+        remixedRoutineBuilder.reverse();
+        return remixedRoutineBuilder.toString();
+    }
 }
---
+++ ZingZingRevolutionREMIX.md
@@ -0,0 +1,1 @@
+![alt text](BoohbahDanceLabOutput2.png)
